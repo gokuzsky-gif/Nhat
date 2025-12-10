0fa28610fa4a18fbff2a6b8b2eaf8d7a09d08b35
QUANG NGO <QUANG NGO@LAPTOP-P8UD2OAB>
2025-12-10 23:22:26 +0700

    Add project files.

---
+++ Nhat.Module.Web/WebModule.Designer.vb
@@ -0,0 +1,39 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+
+Partial Public Class NhatAspNetModule
+    ''' <summary>
+    ''' Required designer variable.
+    ''' </summary>
+    Private components As System.ComponentModel.IContainer = Nothing
+
+    ''' <summary>
+    ''' Clean up any resources being used.
+    ''' </summary>
+    ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso (Not components Is Nothing) Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+#Region "Component Designer generated code"
+
+    ''' <summary>
+    ''' Required method for Designer support - do not modify
+    ''' the contents of this method with the code editor.
+    ''' </summary>
+    Private Sub InitializeComponent()
+        '
+        'NhatAspNetModule
+        '
+        Me.RequiredModuleTypes.Add(GetType(Nhat.Module.NhatModule))
+        Me.RequiredModuleTypes.Add(GetType(DevExpress.ExpressApp.Web.SystemModule.SystemAspNetModule))
+        Me.RequiredModuleTypes.Add(GetType(DevExpress.ExpressApp.ReportsV2.Web.ReportsAspNetModuleV2))
+        Me.RequiredModuleTypes.Add(GetType(DevExpress.ExpressApp.Validation.Web.ValidationAspNetModule))
+
+    End Sub
+
+#End Region
+End Class
---
+++ Nhat.Web/WebApplication.vb
@@ -0,0 +1,142 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports DevExpress.ExpressApp
+Imports System.ComponentModel
+Imports DevExpress.ExpressApp.Web
+Imports System.Collections.Generic
+Imports DevExpress.ExpressApp.Xpo
+Imports DevExpress.ExpressApp.Security
+Imports DevExpress.ExpressApp.Security.ClientServer
+
+' For more typical usage scenarios, be sure to check out https://docs.devexpress.com/eXpressAppFramework/DevExpress.ExpressApp.Web.WebApplication
+Partial Public Class NhatAspNetApplication
+    Inherits WebApplication
+    Private module1 As DevExpress.ExpressApp.SystemModule.SystemModule
+    Private module2 As DevExpress.ExpressApp.Web.SystemModule.SystemAspNetModule
+    Private module3 As Nhat.Module.NhatModule
+    Private module4 As Nhat.Module.Web.NhatAspNetModule
+
+    Private securityModule1 As DevExpress.ExpressApp.Security.SecurityModule
+    Private securityStrategyComplex1 As DevExpress.ExpressApp.Security.SecurityStrategyComplex
+    Private authenticationStandard1 As DevExpress.ExpressApp.Security.AuthenticationStandard
+    Private objectsModule As DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule
+    Private conditionalAppearanceModule As DevExpress.ExpressApp.ConditionalAppearance.ConditionalAppearanceModule
+    Private reportsModuleV2 As DevExpress.ExpressApp.ReportsV2.ReportsModuleV2
+    Private reportsAspNetModuleV2 As DevExpress.ExpressApp.ReportsV2.Web.ReportsAspNetModuleV2
+    Private validationModule As DevExpress.ExpressApp.Validation.ValidationModule
+    Private validationAspNetModule As DevExpress.ExpressApp.Validation.Web.ValidationAspNetModule
+
+    Public Sub New()
+        InitializeComponent()
+    End Sub
+
+    Protected Overrides Function CreateViewUrlManager() As IViewUrlManager
+        Return New ViewUrlManager()
+    End Function
+
+    Protected Overrides Sub CreateDefaultObjectSpaceProvider(ByVal args As CreateCustomObjectSpaceProviderEventArgs)
+        args.ObjectSpaceProvider = New SecuredObjectSpaceProvider(DirectCast(Me.Security, ISelectDataSecurityProvider), GetDataStoreProvider(args.ConnectionString, args.Connection), True)
+        args.ObjectSpaceProviders.Add(New NonPersistentObjectSpaceProvider(TypesInfo, Nothing))
+    End Sub
+    Private Function GetDataStoreProvider(ByVal connectionString As String, ByVal connection As System.Data.IDbConnection) As IXpoDataStoreProvider
+        Dim application As System.Web.HttpApplicationState = If((System.Web.HttpContext.Current IsNot Nothing), System.Web.HttpContext.Current.Application, Nothing)
+        Dim dataStoreProvider As IXpoDataStoreProvider = Nothing
+        If Not application Is Nothing AndAlso application("DataStoreProvider") IsNot Nothing Then
+            dataStoreProvider = TryCast(application("DataStoreProvider"), IXpoDataStoreProvider)
+        Else
+            dataStoreProvider = XPObjectSpaceProvider.GetDataStoreProvider(connectionString, connection, True)
+            If Not application Is Nothing Then
+                application("DataStoreProvider") = dataStoreProvider
+            End If
+        End If
+        Return dataStoreProvider
+    End Function
+    Private Sub NhatAspNetApplication_DatabaseVersionMismatch(ByVal sender As Object, ByVal e As DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs) Handles MyBase.DatabaseVersionMismatch
+#If EASYTEST Then
+        e.Updater.Update()
+        e.Handled = True
+#Else
+        If System.Diagnostics.Debugger.IsAttached Then
+            e.Updater.Update()
+            e.Handled = True
+        Else
+            Dim message As String = "The application cannot connect to the specified database, " & _
+                "because the database doesn't exist, its version is older " & _
+                "than that of the application or its schema does not match " & _
+                "the ORM data model structure. To avoid this error, use one " & _
+                "of the solutions from the https://www.devexpress.com/kb=T367835 KB Article."
+
+            If e.CompatibilityError IsNot Nothing AndAlso e.CompatibilityError.Exception IsNot Nothing Then
+                message &= Constants.vbCrLf & Constants.vbCrLf & "Inner exception: " & e.CompatibilityError.Exception.Message
+            End If
+            Throw New InvalidOperationException(message)
+        End If
+#End If
+    End Sub
+    Private Sub InitializeComponent()
+        Me.module1 = New DevExpress.ExpressApp.SystemModule.SystemModule()
+        Me.module2 = New DevExpress.ExpressApp.Web.SystemModule.SystemAspNetModule()
+        Me.module3 = New Nhat.Module.NhatModule()
+        Me.module4 = New Nhat.Module.Web.NhatAspNetModule()
+        Me.securityModule1 = New DevExpress.ExpressApp.Security.SecurityModule()
+        Me.securityStrategyComplex1 = New DevExpress.ExpressApp.Security.SecurityStrategyComplex()
+        Me.securityStrategyComplex1.SupportNavigationPermissionsForTypes = False
+        Me.authenticationStandard1 = New DevExpress.ExpressApp.Security.AuthenticationStandard()
+        Me.objectsModule = New DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule()
+        Me.conditionalAppearanceModule = New DevExpress.ExpressApp.ConditionalAppearance.ConditionalAppearanceModule()
+        Me.reportsModuleV2 = New DevExpress.ExpressApp.ReportsV2.ReportsModuleV2()
+        Me.reportsAspNetModuleV2 = New DevExpress.ExpressApp.ReportsV2.Web.ReportsAspNetModuleV2()
+        Me.validationModule = New DevExpress.ExpressApp.Validation.ValidationModule()
+        Me.validationAspNetModule = New DevExpress.ExpressApp.Validation.Web.ValidationAspNetModule()
+        CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+        '
+        ' securityStrategyComplex1
+        '
+        Me.securityStrategyComplex1.Authentication = Me.authenticationStandard1
+        Me.securityStrategyComplex1.RoleType = GetType(DevExpress.Persistent.BaseImpl.PermissionPolicy.PermissionPolicyRole)
+        ' ApplicationUser descends from PermissionPolicyUser and supports OAuth authentication. For more information, refer to the following help topic: https://docs.devexpress.com/eXpressAppFramework/402197
+        ' If your application uses PermissionPolicyUser or a custom user type, set the UserType property as follows:
+        Me.securityStrategyComplex1.UserType = GetType(Nhat.Module.BusinessObjects.ApplicationUser)
+        '
+        ' securityModule1
+        '
+        Me.securityModule1.UserType = GetType(Nhat.Module.BusinessObjects.ApplicationUser)
+        '
+        ' authenticationStandard1
+        '
+        Me.authenticationStandard1.LogonParametersType = GetType(DevExpress.ExpressApp.Security.AuthenticationStandardLogonParameters)
+        ' ApplicationUserLoginInfo is only necessary for applications that use the ApplicationUser user type.
+        ' Comment out the following line if using PermissionPolicyUser or a custom user type.
+        Me.authenticationStandard1.UserLoginInfoType = GetType(Nhat.Module.BusinessObjects.ApplicationUserLoginInfo)
+        '
+        ' reportsModuleV2
+        '
+        Me.reportsModuleV2.EnableInplaceReports = True
+        Me.reportsModuleV2.ReportDataType = GetType(DevExpress.Persistent.BaseImpl.ReportDataV2)
+        Me.reportsAspNetModuleV2.ReportViewerType = DevExpress.ExpressApp.ReportsV2.Web.ReportViewerTypes.HTML5
+        Me.reportsModuleV2.ReportStoreMode = DevExpress.ExpressApp.ReportsV2.ReportStoreModes.XML
+        '
+        ' validationModule
+        '
+        Me.validationModule.AllowValidationDetailsAccess = False
+        '
+        ' NhatAspNetApplication
+        '
+        Me.ApplicationName = "Nhat"
+        Me.CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema
+        Me.Modules.Add(Me.module1)
+        Me.Modules.Add(Me.module2)
+        Me.Modules.Add(Me.module3)
+        Me.Modules.Add(Me.module4)
+        Me.Modules.Add(Me.securityModule1)
+        Me.Security = Me.securityStrategyComplex1
+        Me.Modules.Add(Me.objectsModule)
+        Me.Modules.Add(Me.conditionalAppearanceModule)
+        Me.Modules.Add(Me.reportsModuleV2)
+        Me.Modules.Add(Me.reportsAspNetModuleV2)
+        Me.Modules.Add(Me.validationModule)
+        Me.Modules.Add(Me.validationAspNetModule)
+        CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+    End Sub
+End Class
+
